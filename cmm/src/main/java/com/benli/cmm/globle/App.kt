65f13c06a0caab52d1797cb7ba37bbb6a37fc3e0
benli.shi <vipsbl@sina.cn>
2017-08-17 13:52:25 +0800

    添加multiDex支持

--- cmm/src/main/java/com/benli/cmm/globle/App.kt
+++ cmm/src/main/java/com/benli/cmm/globle/App.kt
@@ -1,12 +1,13 @@
 package com.benli.cmm.globle
 
 import android.app.Application
+import android.support.multidex.MultiDexApplication
 import android.util.DisplayMetrics
 
 /**
  * Created by shibenli on 2017/7/31.
  */
-open class App : Application() {
+open class App : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
         app = this
